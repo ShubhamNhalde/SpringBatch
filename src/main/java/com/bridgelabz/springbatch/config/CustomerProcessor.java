eb2fc9c9991ef2129cd069737e48a0a7edd3b09f
Shubham <nhaldes@gmail.com>
2022-05-20 17:10:09 +0530

    ADDED: Storing CSV data in MYSQL database

---
+++ src/main/java/com/bridgelabz/springbatch/config/CustomerProcessor.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.springbatch.config;
+
+import com.bridgelabz.springbatch.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        return customer;
+    }
+}
+
+
+// if (customer.getCountry().equals("United States")) {
+//         return customer;
+//         } else {
+//         return null;
+//         }
